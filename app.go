6dc38d034c875a9320329d9c9a743717a0c0a308
Olga Lavtar <olavtar@redhat.com>
2022-04-07 15:52:47 -0700

    working version of creating secrets and inventories

--- app.go
+++ app.go
@@ -5,9 +5,11 @@ import (
 	"flag"
 	"fmt"
 	dbaasv1alpha1 "github.com/RHEcosystemAppEng/dbaas-operator/api/v1alpha1"
+	"gopkg.in/yaml.v3"
 	core "k8s.io/api/core/v1"
 	apiserver "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -31,7 +33,6 @@ func main() {
 	fmt.Println("Running")
 	test()
 	var err error
-	var c client.Client
 	var config *rest.Config
 	if os.Getenv("KUBERNETES_SERVICE_HOST") == "" {
 		var kubeconfig *string
@@ -76,11 +77,17 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+	scheme := runtime.NewScheme()
+	if err := dbaasv1alpha1.AddToScheme(scheme); err != nil {
+		fmt.Printf("Failed to create schema.", err)
+	}
+	c, err := client.New(config, client.Options{Scheme: scheme})
 	ciSecret, err := clientset.CoreV1().Secrets("osde2e-ci-secrets").Get(context.TODO(), "ci-secrets", meta.GetOptions{})
 	if err != nil {
 		fmt.Println("Error getting ciSecret", err)
 	} else {
 		fmt.Println("ciSecret Found: ")
+		namespace := "openshift-dbaas-operator"
 		//get the list of providers by getting providerList secret
 		if providerListSecret, ok := ciSecret.Data["providerList"]; ok {
 			fmt.Printf("providerListSecret = %s, ok = %v\n", providerListSecret, ok)
@@ -90,17 +97,14 @@ func main() {
 			for _, providerName := range providers {
 				fmt.Println(providerName)
 				var secretData = make(map[string][]byte)
-				namespace := "openshift-dbaas-operator"
 				for key, value := range ciSecret.Data {
 					if strings.HasPrefix(key, providerName) {
 						fmt.Printf("    %s: %s\n", key, value)
 						var keyName = strings.Split(key, "-")
-						fmt.Println(keyName[1])
 						//create map of secret data
 						secretData[keyName[1]] = value
 					}
 				}
-				fmt.Println(secretData)
 				//create secret
 				secret := core.Secret{
 					TypeMeta: meta.TypeMeta{
@@ -113,8 +117,9 @@ func main() {
 					},
 					Data: secretData,
 				}
-				if _, err := clientset.CoreV1().Secrets("openshift-dbaas-operator").Create(context.TODO(), &secret, meta.CreateOptions{}); err != nil {
-					fmt.Printf("Failed to create secret for : %v", err)
+				logYaml(secret)
+				if _, err = clientset.CoreV1().Secrets("openshift-dbaas-operator").Create(context.TODO(), &secret, meta.CreateOptions{}); err != nil {
+					fmt.Printf("Failed to create secret for : %v\n", err)
 				}
 
 				//create inventory
@@ -124,7 +129,7 @@ func main() {
 						APIVersion: "dbaas.redhat.com/v1alpha1",
 					},
 					ObjectMeta: meta.ObjectMeta{
-						Name:      "providerAcct-test-e2e" + providerName,
+						Name:      "provider-acct-test-e2e-" + providerName,
 						Namespace: namespace,
 						Labels:    map[string]string{"related-to": "dbaas-operator", "type": "dbaas-vendor-service"},
 					},
@@ -141,9 +146,9 @@ func main() {
 						},
 					},
 				}
-				fmt.Println(inventory)
+				logYaml(inventory)
 				if err = c.Create(context.Background(), &inventory); err != nil {
-					fmt.Printf("Failed to create invenotry for : %v", err)
+					fmt.Printf("Failed to create invenotry for : %v\n", err)
 				}
 
 			}
@@ -162,4 +167,10 @@ func main() {
 		//	fmt.Printf("    %s: %s\n", key, value)
 		//}
 	}
+}
+
+func logYaml(object interface{}) {
+	data, _ := yaml.Marshal(object)
+	fmt.Println("Data: ")
+	fmt.Println(string(data))
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/RHEcosystemAppEng/dbaas-operator v0.1.5-0.20220329142537-6708678c607a
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/api v0.22.1
 	k8s.io/apiextensions-apiserver v0.22.1
 	k8s.io/apimachinery v0.22.1
