884da4f9acb31279231e0eb42bb1e07e426ebc27
Olga Lavtar <olavtar@redhat.com>
2022-04-20 11:23:35 -0700

    Trying to implement It() inside For loop
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- pkg/rhoda/rhoda_test.go
+++ pkg/rhoda/rhoda_test.go
@@ -60,114 +60,111 @@ var _ = Describe("Rhoda e2e Test", func() {
 	})
 
 	Context("Populate providers array, create secret", func() {
-		It("Get list of providers from the vault, create secrets, populate array", func() {
-			//When("Getting the providers list from the ci-secrets", func() {
-			//Get ci-secret's data
-			clientset, err := kubernetes.NewForConfig(config)
-			Expect(err).NotTo(HaveOccurred())
+		//	It("Get list of providers from the vault, create secrets, populate array", func() {
+		//When("Getting the providers list from the ci-secrets", func() {
+		//Get ci-secret's data
+		clientset, err := kubernetes.NewForConfig(config)
+		Expect(err).NotTo(HaveOccurred())
 
-			ciSecret, err := clientset.CoreV1().Secrets("osde2e-ci-secrets").Get(context.TODO(), "ci-secrets", meta.GetOptions{})
-			Expect(err).NotTo(HaveOccurred())
+		ciSecret, err := clientset.CoreV1().Secrets("osde2e-ci-secrets").Get(context.TODO(), "ci-secrets", meta.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
 
-			fmt.Println("ciSecret Found: ")
-			//get the list of providers by getting providerList secret
-			var providerNames []string
-			if providerListSecret, ok := ciSecret.Data["providerList"]; ok {
-				fmt.Printf("providerListSecret = %s, ok = %v\n", providerListSecret, ok)
-				providerNames = strings.Split(string(providerListSecret), ",")
-				fmt.Println(providerNames)
-			}
+		fmt.Println("ciSecret Found: ")
+		//get the list of providers by getting providerList secret
+		var providerNames []string
+		if providerListSecret, ok := ciSecret.Data["providerList"]; ok {
+			fmt.Printf("providerListSecret = %s, ok = %v\n", providerListSecret, ok)
+			providerNames = strings.Split(string(providerListSecret), ",")
+			fmt.Println(providerNames)
+		}
 
-			//loop through providers to create secrets
-			for _, providerName := range providerNames {
-				//	When("Creating secret for "+providerName, func() {
-				It("create SecretData and a secret", func() {
-					fmt.Println("Creating secret")
-					fmt.Println(providerName)
-					var secretData = make(map[string][]byte)
-					for key, value := range ciSecret.Data {
-						if strings.HasPrefix(key, providerName) {
-							fmt.Printf("    %s: %s\n", key, value)
-							var keyName = strings.Split(key, "-")
-							//create map of secret data
-							secretData[keyName[1]] = value
-						}
+		//loop through providers to create secrets
+		for _, providerName := range providerNames {
+			//	When("Creating secret for "+providerName, func() {
+			It("create SecretData and a secret", func() {
+				fmt.Println("Creating secret")
+				fmt.Println(providerName)
+				var secretData = make(map[string][]byte)
+				for key, value := range ciSecret.Data {
+					if strings.HasPrefix(key, providerName) {
+						fmt.Printf("    %s: %s\n", key, value)
+						var keyName = strings.Split(key, "-")
+						//create map of secret data
+						secretData[keyName[1]] = value
 					}
+				}
 
-					//create secret
-					secret := core.Secret{
-						TypeMeta: meta.TypeMeta{
-							Kind:       "Secret",
-							APIVersion: "v1",
-						},
-						ObjectMeta: meta.ObjectMeta{
-							Name:      "dbaas-secret-e2e-" + providerName,
-							Namespace: namespace,
-						},
-						Data: secretData,
-					}
-					_, err = clientset.CoreV1().Secrets("openshift-dbaas-operator").Create(context.TODO(), &secret, meta.CreateOptions{})
-					Expect(err).NotTo(HaveOccurred())
+				//create secret
+				secret := core.Secret{
+					TypeMeta: meta.TypeMeta{
+						Kind:       "Secret",
+						APIVersion: "v1",
+					},
+					ObjectMeta: meta.ObjectMeta{
+						Name:      "dbaas-secret-e2e-" + providerName,
+						Namespace: namespace,
+					},
+					Data: secretData,
+				}
+				_, err = clientset.CoreV1().Secrets("openshift-dbaas-operator").Create(context.TODO(), &secret, meta.CreateOptions{})
+				Expect(err).NotTo(HaveOccurred())
 
-					//add to array
-					providers = append(providers, rhoda.ProviderAccount{ProviderName: providerName, SecretName: "dbaas-secret-e2e-" + providerName, SecretData: secretData})
-				})
-				//	})
-			}
-		})
+				//add to array
+				providers = append(providers, rhoda.ProviderAccount{ProviderName: providerName, SecretName: "dbaas-secret-e2e-" + providerName, SecretData: secretData})
+			})
+			//	})
+		}
 	})
 
 	Context("Create Inventory", func() {
-		It("Adding dbaas scheme to create an inventory", func() {
-			//	When("Adding dbaas scheme to create an inventory", func() {
-			scheme := runtime.NewScheme()
-			err := dbaasv1alpha1.AddToScheme(scheme)
-			Expect(err).NotTo(HaveOccurred())
+		scheme := runtime.NewScheme()
+		err := dbaasv1alpha1.AddToScheme(scheme)
+		Expect(err).NotTo(HaveOccurred())
 
-			c, err = client.New(config, client.Options{Scheme: scheme})
-			Expect(err).NotTo(HaveOccurred())
+		c, err = client.New(config, client.Options{Scheme: scheme})
+		Expect(err).NotTo(HaveOccurred())
 
-			for _, value := range providers {
-				//		When("Creating inventory for "+value.ProviderName, func() {
-				It("Creating inventory", func() {
-					fmt.Println("Creating inventory")
-					fmt.Println(value.ProviderName)
-					fmt.Println(value.SecretName)
-					//for k, v := range value.SecretData {
-					//	fmt.Printf("    %s: %s\n", k, v)
-					//}
+		for _, value := range providers {
+			//		When("Creating inventory for "+value.ProviderName, func() {
+			It("Creating inventory", func() {
+				fmt.Println("Creating inventory")
+				fmt.Println(value.ProviderName)
+				fmt.Println(value.SecretName)
+				//for k, v := range value.SecretData {
+				//	fmt.Printf("    %s: %s\n", k, v)
+				//}
 
-					//create inventory
-					inventory := dbaasv1alpha1.DBaaSInventory{
-						TypeMeta: meta.TypeMeta{
-							Kind:       "DBaaSInventory",
-							APIVersion: "dbaas.redhat.com/v1alpha1",
-						},
-						ObjectMeta: meta.ObjectMeta{
-							Name:      "provider-acct-test-e2e-" + value.ProviderName,
+				//create inventory
+				inventory := dbaasv1alpha1.DBaaSInventory{
+					TypeMeta: meta.TypeMeta{
+						Kind:       "DBaaSInventory",
+						APIVersion: "dbaas.redhat.com/v1alpha1",
+					},
+					ObjectMeta: meta.ObjectMeta{
+						Name:      "provider-acct-test-e2e-" + value.ProviderName,
+						Namespace: namespace,
+						Labels:    map[string]string{"related-to": "dbaas-operator", "type": "dbaas-vendor-service"},
+					},
+					Spec: dbaasv1alpha1.DBaaSOperatorInventorySpec{
+						ProviderRef: dbaasv1alpha1.NamespacedName{
 							Namespace: namespace,
-							Labels:    map[string]string{"related-to": "dbaas-operator", "type": "dbaas-vendor-service"},
+							Name:      string(value.SecretData["providerType"]),
 						},
-						Spec: dbaasv1alpha1.DBaaSOperatorInventorySpec{
-							ProviderRef: dbaasv1alpha1.NamespacedName{
+						DBaaSInventorySpec: dbaasv1alpha1.DBaaSInventorySpec{
+							CredentialsRef: &dbaasv1alpha1.NamespacedName{
 								Namespace: namespace,
-								Name:      string(value.SecretData["providerType"]),
-							},
-							DBaaSInventorySpec: dbaasv1alpha1.DBaaSInventorySpec{
-								CredentialsRef: &dbaasv1alpha1.NamespacedName{
-									Namespace: namespace,
-									Name:      "dbaas-secret-e2e-" + value.ProviderName,
-								},
+								Name:      "dbaas-secret-e2e-" + value.ProviderName,
 							},
 						},
-					}
-					err = c.Create(context.Background(), &inventory)
-					Expect(err).NotTo(HaveOccurred())
-				})
-				//	})
-			}
-		})
+					},
+				}
+				err = c.Create(context.Background(), &inventory)
+				Expect(err).NotTo(HaveOccurred())
+			})
+			//	})
+		}
 	})
+
 	Context("Check inventories status", func() {
 		It("Should pass when the inventory is processed", func() {
 			for _, value := range providers {
